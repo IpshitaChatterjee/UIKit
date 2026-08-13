6d3f2d37bcbea812a25936c6e74159a51f65d19c
IpshitaChatterjee <IpshitaChatterjee@users.noreply.github.com>
2026-08-13 13:24:56 +0000

    deploy: 3606c864c5c2b0e5953e0e8969c1265aa0f2f684

---
+++ assets/Tokens.stories-CImsHdvx.js
@@ -0,0 +1,6 @@
+import{n as e}from"./iframe-DepWYGwJ.js";import{n as t}from"./rolldown-runtime-DkW27tQK.js";var n,r,i,a;function o(){return(o=t((()=>{n=e(),r={title:`Design Tokens`},i=()=>(0,n.jsx)(`div`,{style:{padding:`24px`,fontFamily:`sans-serif`},children:(0,n.jsx)(`p`,{children:`Design tokens live in the panel below (look for the "Design Tokens" tab).`})}),i.__docgenInfo={description:``,methods:[],displayName:`Tokens`},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`() => <div style={{
+  padding: '24px',
+  fontFamily: 'sans-serif'
+}}>
+    <p>Design tokens live in the panel below (look for the "Design Tokens" tab).</p>
+  </div>`,...i.parameters?.docs?.source}}},a=[`Tokens`]})))()}o();export{i as Tokens,a as __namedExportsOrder,r as default};
---
+++ assets/iframe-CNsrdUpb.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-DepWYGwJ.js";e();
